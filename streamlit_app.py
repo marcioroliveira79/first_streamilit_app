a5e12e2038f7ae3a5dc889ca91c7e3c24405b446
MARCIO RODRIGUES DE OLIVEIRA <marcio.r.oliveira79@gmail.com>
2023-01-20 01:51:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,5 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-
 streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
