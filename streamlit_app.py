3ac61291f0337dc5c483d07c9ac92dd44e8ce062
MARCIO RODRIGUES DE OLIVEIRA <marcio.r.oliveira79@gmail.com>
2023-01-20 01:45:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Bluberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+import pandas
+
+my_frui_list = panda.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
